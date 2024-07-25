6f4c6aafeed1acc57877edc54c49bc21df6a2d76
nbyanjankar <nbyanjankar@myseneca.ca>
2024-07-25 12:47:41 -0400

    change

--- server.js
+++ server.js
@@ -15,11 +15,37 @@ const path = require('path');
 const collegeData = require("./modules/collegeData");
 const bodyParser = require('body-parser');
 const app = express();
-const HTTP_PORT = process.env.PORT || 8080;
+const fs = require('fs');
+const HTTP_PORT = process.env.PORT || 8080; 
+const exphbs = require('express-handlebars');
 
+const studentsFilePath = path.join(__dirname, 'data', 'students.json');
+let students = [];
 
 app.set('views', __dirname + '/views');
 
+const hbs = exphbs.create({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+        navLink: function(url, options) {
+            return '<li' +
+                ((url == app.locals.activeRoute) ? ' class="nav-item active" ' : ' class="nav-item" ') +
+                '><a class="nav-link" href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }
+    }
+});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 // Serve static files from the 'Public' directory
 app.use(express.static(path.join(__dirname, 'Public')));
@@ -42,35 +68,63 @@ collegeData.initialize()
 
 // Route for home.html
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+//     res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.render('home');
 });
+
 
 // Route for about.html (assuming you create this file)
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));
+//     res.sendFile(path.join(__dirname, '/views/about.html'));
+    res.render('about');
 });
 
 // Route for htmlDemo.html (assuming you create this file)
 app.get("/htmlDemo", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/htmldemo.html'));
+    // res.sendFile(path.join(__dirname, '/views/htmldemo.html'));
+    res.render('htmlDemo');
 });
 
 app.get("/students/add", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/addStudent.html'));
+    // res.sendFile(path.join(__dirname, '/views/addStudent.html'));
+    res.render('addStudent');
 });
 
+app.get("/singlestudent", (req, res) => {
+    res.render('singlestd');
+});
   
-  
+
+// middleware
+app.use(function(req, res, next) {
+    let route = req.path.substring(1);
+    app.locals.activeRoute = "/" + (isNaN(route.split('/')[1]) ? route.replace(/\/(?!.*)/, "") : route.replace(/\/(.*)/, ""));
+    next();
+});
+
+
+
 
 app.get('/students', (req, res) => {
-    collegeData.getAllStudents()
-        .then((students) => {
-            res.json(students);
-        })
-        .catch((err) => {
-            res.status(500).json({ message: "Error fetching students", error: err });
-        });
+    fs.readFile('./data/students.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.render('students', { message: "no results" });
+        }
+        let students = JSON.parse(data);
+        res.render('students', { students: students });
+    });
 });
+
+app.get('/courses', (req, res) => {
+    fs.readFile('./data/courses.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.render('courses', { message: "no results" });
+        }
+        let courses = JSON.parse(data);
+        res.render('courses', { courses: courses });
+    });
+});
+
 
 
 // Example route for /students by course
@@ -95,17 +149,57 @@ app.get("/studentscourse", (req, res) => {
     }
 });
 
-// Example route for a single student by student number
-app.get("/student/:studentNum", (req, res) => {
-    const num = paresInt(req.params.numuh)
-    collegeData.getStudentByNum(req.params.studentNum)
-        .then((student) => {
-            res.json(student);
-        })
-        .catch((err) => {
-            res.status(500).json({ message: "Error fetching student", error: err });
-        });
+fs.readFile(studentsFilePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading students file:', err);
+        return;
+    }
+    students = JSON.parse(data);
 });
+app.use((req, res, next) => {
+    res.locals.students = students;
+    next();
+});
+
+app.get('/student', (req, res) => {
+    const studentNum = parseInt(req.query.studentNum, 10); // Convert input to integer
+    if (isNaN(studentNum)) {
+        return res.render('student', { message: 'Invalid student number' });
+    }
+
+    const student = students.find(s => s.studentNum === studentNum);
+
+    if (student) {
+        res.render('student', { student });
+    } else {
+        res.render('student', { message: 'Student not found' });
+    }
+});
+
+app.get('/student', (req, res) => {
+    console.log('Query Parameters:', req.query); // Log query parameters
+    
+    const studentNum = parseInt(req.query.studentNum, 10); // Convert input to integer
+    console.log('Converted studentNum:', studentNum); // Log converted student number
+
+    if (isNaN(studentNum)) {
+        return res.render('student', { message: 'Invalid student number' });
+    }
+
+    const student = students.find(s => s.studentNum === studentNum);
+    console.log('Found student:', student); // Log the student object if found
+
+    if (student) {
+        res.render('student', { student });
+    } else {
+        res.render('student', { message: 'Student not found' });
+    }
+});
+
+
+
+
+
 
 // Example route for /managers (assuming you have this function)
 app.get("/managers", (req, res) => {
@@ -118,16 +212,7 @@ app.get("/managers", (req, res) => {
         });
 });
 
-// Example route for /courses
-app.get("/courses", (req, res) => {
-    collegeData.getCourses()
-        .then((courses) => {
-            res.json(courses);
-        })
-        .catch((err) => {
-            res.status(500).json({ message: "Error fetching courses", error: err });
-        });
-});
+
 
 // Route to handle form submission
 app.post('/students/add', (req, res) => {
